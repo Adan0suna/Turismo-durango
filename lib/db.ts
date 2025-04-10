c5012241588affe93f5b94d2ff4cd81c06a1cd7f
Adan0suna <gabrielosuna240@gmail.com>
2025-04-10 10:03:01 -0600

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,9 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
+  host: 'db-turismo2025mysql.cv42waa86l8l.us-west-1.rds.amazonaws.com',
+  user: 'Jairo-DEV',
+  password: 'Jairo123456789',
   database: 'test',
 });
 
